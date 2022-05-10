1a3edc362730a296ce3ebbec9473edccb7709d98
TingtingSsl2 <31963567+TingtingSsl2@users.noreply.github.com>
2022-05-10 14:34:14 -0400

    update

--- cellranger_count.sh
+++ cellranger_count.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-annotation=$1
 conda activate scrnaseq
-module load cellranger/6.1.1
-cellranger count  --id=$annotation \
+module load cellranger/6.0
+cellranger count  --id=w3merged \
                   --transcriptome=/data/bioinformatics/referenceGenome/Drosophila_Melanogaster/Ensembl/dm6/Drosophila_melanogaster.genome \
-                  --fastqs=/data/bioinformatics/projects/hassan2022/output/01_cellranger_mkfastq/outs/fastq_path/ \
-                  --sample=$annotation \
-		  --include-introns \
-		  --localcores=8 \
-		  --localmem=64
-
-
+                  --fastqs=/data/bioinformatics/projects/hassan2022/output/01_cellranger_mkfastq/outs/fastq_path/H72NCDMXY/,/data/bioinformatics/projects/rachel2022/output/01_cellranger_mkfastq/outs/fastq_path/H7KGTDMXY/ \
+                  --sample=CRN00224925,w3 \
+                  --expect-cells=10000
 
